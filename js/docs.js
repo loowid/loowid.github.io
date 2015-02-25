ee2844ca0f1c0b271cae326d120ad78745204838
Eduardo Rey <proyectos@seduerey.com>
2015-02-25 17:40:31 +0100

    added docs and smooth scrolling.

--- js/docs.js
+++ js/docs.js
@@ -1,5 +1,23 @@
 $( document ).ready(function() {
-    $('.entry h1').each( function(i){
-    	console.log( $(this).text() );
+    $('body.docs .entry h1').each( function(i){
+    	var href = 'ancla' + i;
+    	var $struct = $('<li><a href="#' + href + '">'+ $(this).text() +'</a></li>');
+    	$('.docs header #navigation').append( $struct );
+    	$(this).append('<a name="' + href + '" />');
     });
+});
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
